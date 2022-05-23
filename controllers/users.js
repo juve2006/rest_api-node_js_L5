7d3da0a9ecb80b4c49d6a2a3ac38b5d9e3743e39
evgen sikora <arnika200@gmail.com>
2022-05-23 21:26:21 +0300

    errorhandlers added

--- controllers/users.js
+++ controllers/users.js
@@ -7,32 +7,47 @@ export const getUsers = (req, res) => {
 }
 
 export const createUser = (req, res) => {
-    const user = req.body;
-    const userId = uuid();
-    const userWithId = {...user, id: userId}; // add id for users
-    users.push(userWithId);
-    res.send(`User ${user.name} added to DB`);
+    try {
+        const user = req.body;
+        const userId = uuid();
+        const userWithId = {...user, id: userId}; // add id for users
+        users.push(userWithId);
+        res.status(201).send(`User ${user.name}  ${user.surname} added to DB`);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 }
 
 export const getUser = (req, res) => {
     const { id } = req.params;
     const userGet = users.find((user) => user.id === id);
+    if (userGet == null) {
+        return res.status(404).send({ message: `Cannot find user with id ${id}`});
+    }
     res.send (userGet);
 }
 
 export const deleteUser = (req, res) => {
-    const { id } = req.params;
-    users = users.filter((user) => user.id !== id);
-    res.send (`User with id:${id} deleted from DB`);
+    try {
+        const { id } = req.params;
+        users = users.filter((user) => user.id !== id);
+        res.send (`User with id:${id} deleted from DB`);
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
 }
 
 export const updateUser = (req, res) => {
-    const { id } = req.params;
-    const { name, surname, email } = req.body;
-    let user = users.find((user) => user.id === id);
+   try {
+       const { id } = req.params;
+       const { name, surname, email } = req.body;
+       let user = users.find((user) => user.id === id);
 
-    if (name) user.name = name;
-    if (surname) user.surname = surname;
-    if (email) user.email = email;
-    res.send(`user ${name} has been updated`);
+       if (name) user.name = name;
+       if (surname) user.surname = surname;
+       if (email) user.email = email;
+       res.send(`user ${name} ${surname} has been updated`);
+   } catch (err) {
+       res.status(400).send({ message: err.message });
+   }
 }
